ed65b9bd65dfec46ed45c25d2ce51353ba1f2a7e
Duchai232 <Duchai232>
2024-05-10 23:01:41 +0700

    reacthook

--- src/components/home/LeftHome.jsx
+++ src/components/home/LeftHome.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import * as React from "react";
 import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
 import DateRangeIcon from "@mui/icons-material/DateRange";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import MenuIcon from "@mui/icons-material/Menu";
-import likePages from "../../data/LikePages.json";
+
 const listItem = [
   {
     id: 1,
@@ -27,19 +27,49 @@ const listItem = [
     icon: AccessTimeIcon,
     text: "Memories",
   },
+  {
+    id: 5,
+    icon: AccessTimeIcon,
+    text: "ABC",
+  },
+  {
+    id: 6,
+    icon: AccessTimeIcon,
+    text: "XYZ",
+  },
+  {
+    id: 7,
+    icon: AccessTimeIcon,
+    text: "CDE",
+  },
+  {
+    id: 8,
+    icon: AccessTimeIcon,
+    text: "FGH",
+  },
 ];
-const LeftHome = () => {
+const LeftHome = ({ likePages }) => {
+  const initNumberList = 4;
+  const [dataList, setDataList] = React.useState([]);
+  const [dataPages, setDataPages] = React.useState(likePages);
+  const [numberList, setNumberList] = React.useState(initNumberList);
+  const [numberPages, setNumberPages] = React.useState(initNumberList);
+  React.useEffect(() => {
+    setDataList(listItem.slice(0, numberList));
+    setDataPages(likePages.slice(0, numberPages));
+  }, [numberList, numberPages]);
   const handleShow = () => {
-    alert("hello");
+    setNumberList(listItem.length);
   };
-  const handleShowPage = () => {
-    alert("page");
+  const handleHide = () => {
+    setNumberList(initNumberList);
   };
+
   return (
     <div className="lefthome-container">
       <div className="topleft">
         <ul>
-          {listItem.map((item) => (
+          {dataList.map((item) => (
             <li key={item.id}>
               <div className="icon">
                 <item.icon />
@@ -48,8 +78,21 @@ const LeftHome = () => {
             </li>
           ))}
         </ul>
-        <button className="btn-see-more" onClick={handleShow}>
-          <span className="text">See more</span>
+
+        <button
+          className="btn-see-more"
+          onClick={() => {
+            {
+              numberList === initNumberList && handleShow();
+            }
+            {
+              numberList === listItem.length && handleHide();
+            }
+          }}
+        >
+          <span className="text">
+            {numberList === initNumberList ? "See more" : "Hide"}
+          </span>
           <span className="icon">
             <KeyboardArrowDownIcon />
           </span>
@@ -64,18 +107,31 @@ const LeftHome = () => {
         </div>
         <div className="list-page">
           <ul>
-            {likePages.map((page) => (
+            {dataPages.map((page) => (
               <li>
                 <img src={page.image} className="image" />
                 <span className="text">{page.pageName}</span>
               </li>
             ))}
           </ul>
-          <button className="btn-see-more" onClick={handleShowPage}>
-            <span className="text">See more</span>
-            <span className="icon">
-              <KeyboardArrowDownIcon />
+          <button
+            className="btn-see-more"
+            onClick={() => {
+              {
+                numberPages === initNumberList &&
+                  setNumberPages(likePages.length);
+              }
+              {
+                numberPages === likePages.length &&
+                  setNumberPages(initNumberList);
+              }
+            }}
+          >
+            <span className="text">
+              {numberPages === initNumberList ? "See more" : "Hide"}
             </span>
+
+            <KeyboardArrowDownIcon />
           </button>
         </div>
       </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,10 +3,11 @@ import "./Home.css";
 import LeftHome from "../../components/home/LeftHome";
 import NewsFeed from "../../components/home/NewsFeed";
 import RightHome from "../../components/home/RightHome";
+import likePages from "../../data/LikePages.json";
 const Home = () => {
   return (
     <div className="home-container">
-      <LeftHome />
+      <LeftHome likePages={likePages} />
       <NewsFeed />
       <RightHome />
     </div>
