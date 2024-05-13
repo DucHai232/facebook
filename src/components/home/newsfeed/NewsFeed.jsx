f9dbbdff9ed53b6c78f7ad33759be5d67ac22d04
Duchai232 <Duchai232>
2024-05-13 22:59:55 +0700

    story

---
+++ src/components/home/newsfeed/NewsFeed.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Stories from "./Stories";
+import PostStatus from "./PostStatus";
+import NewsContent from "./NewsContent";
+import "./NewsFeed.css";
+const NewsFeed = () => {
+  return (
+    <div className="newsfeed-container">
+      <Stories />
+      <PostStatus />
+      <NewsContent />
+    </div>
+  );
+};
+
+export default NewsFeed;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Home.css";
 import LeftHome from "../../components/home/LeftHome";
-import NewsFeed from "../../components/home/NewsFeed";
+import NewsFeed from "../../components/home/newsfeed/NewsFeed";
 import RightHome from "../../components/home/RightHome";
-import likePages from "../../data/LikePages.json";
+import likePages from "../../data/LikePages";
 const Home = () => {
   return (
     <div className="home-container">
